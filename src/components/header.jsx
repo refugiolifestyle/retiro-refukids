43069e2e0a32d65587f9e992570c4e0815e0c170
Luis Enrique Gomes Portugal <luisenriquegomesportugal@gmail.com>
2023-05-27 00:01:54 -0300

    correcao

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -22,7 +22,7 @@ export const HeaderNavigation = ({ current }) => {
     .split('/');
 
   return <div className="max-w-7xl mx-auto px-6 xl:px-0">
-    <div className="flex items-center justify-between h-24">
+    <div className="flex flex-col md:flex-row items-center justify-between h-24">
       <div className="flex-shrink-0">
         <img
           className="h-20"
@@ -30,7 +30,7 @@ export const HeaderNavigation = ({ current }) => {
           alt="Workflow"
         />
       </div>
-      <div className="ml-10 flex items-baseline space-x-4">
+      <div className="ml-10 flex items-baseline space-x-4 my-4">
         {navigation.map((item, itemIdx) =>
           <a
             key={item.title}
