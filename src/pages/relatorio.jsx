93503054a2a194b6b201ff2679e1ea87a8c25015
Luis Enrique Gomes Portugal <luisenriquegomesportugal@gmail.com>
2024-05-26 14:01:53 -0300

    finalizado numeros para sorteio

--- src/pages/relatorio.jsx
+++ src/pages/relatorio.jsx
@@ -1,36 +1,48 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { Page } from '../components/page';
 import { useInscritosService } from '../services/useInscritosService';
 import { useRifaService } from '../services/useRifaService';
 import { useComprovantesService } from '../services/useComprovantesService';
 import { Button } from 'primereact/button';
+import { Toast } from 'primereact/toast';
 
 export default function Relatorio() {
-  const {inscritos} = useInscritosService();
-  const {rifa} = useRifaService()
-  const {comprovantes} = useComprovantesService()
-  
+  const toast = useRef(null);
+  const { inscritos } = useInscritosService();
+  const { rifa } = useRifaService()
+  const { comprovantes } = useComprovantesService()
+
   useEffect(() => {
     window.inscritos = inscritos
-    console.log('Inscritos atualizados', new Date().toLocaleString('pt-br'))
+    window.rifas = rifa
+    window.comprovantes = comprovantes
+
+    console.log('Dados atualizados', new Date().toLocaleString('pt-br'))
   }, [inscritos])
 
-  useEffect(() => {
-    window.rifas = rifa
-    console.log('Rifas atualizados', new Date().toLocaleString('pt-br'))
-  }, [rifa])
+  const gerarNumerosParaSorteio = async () => {
+    let collactor = new Intl.Collator('pt-BR', { numeric: true, usage: 'sort' });
+    let textoParaSorteio = rifa
+      .reduce((a, r) => {
+        let numerosPreparados = Object
+          .entries(r.numeros)
+          .filter(([n, v]) => !!v)
+          .map(([n, v]) => `${n}: ${v.replaceAll(/(\s+|-)/gi, ' ')} (Vendedor(a) ${r.nome})`)
+        return a.concat(numerosPreparados)
+      }, [])
+      .sort((n, n2) => collactor.compare(n, n2))
+      .join('\n')
 
-  useEffect(() => {
-    window.comprovantes = comprovantes
-    console.log('Comprovantes atualizados', new Date().toLocaleString('pt-br'))
-  }, [comprovantes])
+    await navigator.clipboard.writeText(textoParaSorteio)
+    toast.current.show({ severity: 'success', summary: 'Sucesso', detail: 'Lista copiada com sucesso' });
+  }
 
   return <Page title="Relatórios">
-    <div className='flex flex-col justify-center items-center gap-4'>
-      <Button label='Números para Rifa' onClick={async () => {
-        let text = rifa.reduce((a, r) => a.concat(Object.entries(r.numeros).filter(([n, v]) => v !== false).map(([n, v]) => `${n}: ${v.replaceAll(/(\s+|-)/gi, ' ')} (Vendedor(a) ${r.nome} )`)), []).join('\n')
-        await navigator.clipboard.writeText(text)
-      }} />
+    <Toast ref={toast} />
+    <div className='flex flex-col md:flex-row justify-center items-center gap-4'>
+      <Button
+        label='Números para sorteio'
+        onClick={gerarNumerosParaSorteio} />
     </div>
   </Page>
 }
