ad2ae30140dd1dd83beb6a289fe0a3e9b6386560
Luis Enrique Gomes Portugal <luisenriquegomesportugal@gmail.com>
2025-01-26 11:27:09 -0300

    foto tema

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,11 +2,15 @@ import { Page } from '../components/page';
 import { useConfigService } from '../services/useConfigService';
 
 export default function Index() {
-  const { permitirInscricao } = useConfigService();
+  const { permitirInscricao, exibirFotoTema } = useConfigService();
 
   return <Page title="Inicio">
     <div className='flex flex-col justify-center items-center gap-4 h-64'>
-      <img src="tema.png" alt="Tema do retiro" className="w-56" />
+      {
+        exibirFotoTema === true
+          ? <img src="https://firebasestorage.googleapis.com/v0/b/retiro-refukids.appspot.com/o/tema.png?alt=media&token=b3e8f0f3-02dc-44e1-a65d-420a7a8e679c" alt="Tema do retiro" className="h-40" />
+          : null
+      }
       {
         permitirInscricao === true
           ? <a
